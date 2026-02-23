6094e731d586c31e37fbd786007971716ad72c4b
AnkitTt07clear <ankitthakur960ak@gmail.com>
2026-02-23 23:24:07 +0530

    Float and Clear

---
+++ FloatClear/s.js
@@ -0,0 +1,43 @@
+const h2 = document.querySelector("h2");
+
+h2.style.color = "blue";
+
+
+
+const link = document.querySelectorAll("h3");
+//Method 1 
+// para[0].style.color = "red"; 
+
+//Method 2 : using loops 
+for (let i = 0; i < link.length; i++) {
+    link[i].style.color = "black";
+}
+
+
+// for(const i of para){
+//     i.style.backgroundColor = 'lightgreen';
+//     i.style.fontFamily = 'cursive';
+
+// }
+
+// for (const i of para) {
+//     i.style.cssText = `
+//     color: red ;
+//     font-size: 20px;
+//     font-family: cursive;
+//     background-color: lightgreen;
+//     border : 5px double black;
+//     border-radius: 10px;
+//     text-align : center;
+//     `
+// }
+
+link.className = "green-link";
+
+// link.setAttribute('class', 'green-link');
+
+// link.className = "my-link green-link";
+
+// console.log(link.classList);
+
+link.classList.add("green-link");
