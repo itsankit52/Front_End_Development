25cf516c5554dfcb2e4805a5463d1ea58d4ff6ee
AnkitTt07clear <ankitthakur960ak@gmail.com>
2026-03-22 20:38:59 +0530

    Redux Fundamental

--- Redux/Fundamental/script.js
+++ Redux/Fundamental/script.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux'
+console.dir(createStore);
 /*
 let state = {
     count: 0
@@ -8,20 +10,57 @@ let state = {
     state.count = 1
     console.log(state); */
 
-
+/*
 // Redux mathod (Immutable state)
 
 let state = {
-    count: 0
+    count: 0,
+    name: "Piyush Thakur",
+    age: 20
 }
 
 let prevstate = state
 function increase() {
 
-    state = { count: state.count + 1 }
+    state = { ...state, count: state.count + 1 }
+}
+
+function updateName() {
+    state = { ...state, name: "Ankit Thakur" }
+    state = { ...state, count: state.count + 1 }
 }
 
 increase()
 console.log(state);
-increase()
+updateName();
 console.log(state);
+
+*/
+
+let reduxState = {
+    count: 0,
+    name: "Piyush Thakur",
+    age: 20
+}
+
+function reducer(reduxState, action) {
+    if (action.type === "Increase") {
+        console.log(action);
+        return { ...reduxState, count: reduxState.count + 1 }
+    }
+    else if (action.type === "Decrease") {
+        console.log(action);
+        return { ...reduxState, count: reduxState.count - 1 }
+    }
+    else if (action.type === "IncreaseBy") {
+        console.log(action);
+        return { ...reduxState, count: reduxState.count + action.payload }
+    }
+    else {
+        console.log(action);
+        return reduxState
+    }
+}
+
+const store = createStore(reducer)
+console.log(store);
